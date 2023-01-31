a2f5a705fe80733341714ed94e8cfa1264d88b3c
seb <sebi.goodfellow@gmail.com>
2023-01-30 22:49:21 -0500

    - week4 content

--- lectures/week4/lecture1/notebooks/utils.py
+++ lectures/week4/lecture1/notebooks/utils.py
@@ -1,3 +1,5 @@
+import time
+
 def continue_walking(turtle):
     """
     (turtle) --> bool
@@ -11,3 +13,38 @@ def continue_walking(turtle):
         return False
     else:
         return True
+
+
+def train_ai_model():
+
+    print('Downloading data from cloud...')
+    time.sleep(3)
+    print('Download complete.\n')
+    print('Prepare data for training...')
+    time.sleep(3)
+    print('Preprocessing complete.\n')
+    print('Training AI model...')
+    time.sleep(1)
+    print('10% complete...')
+    time.sleep(1)
+    print('20% complete...')
+    time.sleep(1)
+    print('30% complete...')
+    time.sleep(1)
+    print('40% complete...')
+    time.sleep(1)
+    print('50% complete...')
+    time.sleep(1)
+    print('60% complete...')
+    time.sleep(1)
+    print('70% complete...')
+    time.sleep(1)
+    print('80% complete...')
+    time.sleep(1)
+    print('90% complete...')
+    time.sleep(1)
+    print('100% complete...\n')
+    print('Uploading final model and results to the cloud...')
+    time.sleep(3)
+    print('Upload complete.\n')
+    print('Training complete.')
