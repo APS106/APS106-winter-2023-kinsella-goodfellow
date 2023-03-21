2f07bf9d3002734ea44a753c55c9140a9799ff68
seb <sebi.goodfellow@gmail.com>
2023-03-20 22:42:31 -0400

    - week10 content

---
+++ lectures/week8/lecture3/notebooks/utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pylab as plt
+import seaborn as sns
+
+plt.style.use('fivethirtyeight')
+
+
+def plot_scores(scores):
+
+    scores = pd.DataFrame(data=scores, index=[0])
+
+    plt.figure(figsize=(15, 5))
+    plt.title('Term Test 2 Review Jeopardy Scores', fontsize=26)
+    ax = sns.barplot(x=scores.columns, y=scores.iloc[0])
+    ax.xaxis.set_tick_params(labelsize=24)
+    ax.yaxis.set_tick_params(labelsize=16)
+    ax.set_ylabel('Scores', fontsize=24)
